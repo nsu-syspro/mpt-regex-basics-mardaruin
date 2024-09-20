5aac2e5a379f12b7d7f6e7fdb1da7a24fbc9332b
Marina Rudometova <m.rudometova@g.nsu.ru>
2024-09-20 19:44:51 +0700

    understoooood

--- solution/task5.sh
+++ solution/task5.sh
@@ -1,2 +1,5 @@
 cd data
-echo "$(cat students_new.txt)" | sed -E -r 's/(.*) (.*)(.*),(.*)/\2 \1 (\4)/g' > students.txt
+cp students.csv students_new.txt
+echo "$(cat students_new.txt)" | sed -E -r 's/(.*) (.*),(.*)/\1 \2 \3/g' > students_nnew.txt
+echo "$(cat students_nnew.txt)" | sed -E -r 's/(.*) (.*) (.*) (.*)/\1 \2 \4/g' > students_nnnew.txt
+echo "$(cat students_nnnew.txt)" | sed -E -r 's/(.*) (.*) (.*)/\2 \1 (\3)/g' > students.txt
