6f78f69084ab9078a0baa5ca5a2d32bf111345bb
Marina Rudometova <m.rudometova@g.nsu.ru>
2024-09-20 18:34:37 +0700

    cp added

--- solution/task4.sh
+++ solution/task4.sh
@@ -1,3 +1,5 @@
 cd data
-sed -i 's/Живи/Не спи/g' blok.txt > blok_new.txt 
-sed -i 's/Умрёшь/Уснёшь/g' blok_new.txt > blok_kids_edition.txt
+cp blok.txt blok_new.txt
+sed -i 's/Живи/Не спи/g' blok_new.txt 
+sed -i 's/Умрёшь/Уснёшь/g' blok_new.txt 
+cp blok_new.txt blok_kids_edition.txt
